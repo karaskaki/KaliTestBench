04f655aa680a0fb54c3d483f5e3e9f331540b546
Nikhil <nik.ranchi7@gmail.com>
2021-12-19 23:13:24 +0530

    Changes made.

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('I am Stanish!')
+    #He is a cool guy!
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
