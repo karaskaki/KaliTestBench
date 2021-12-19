16dd9f32fe963ad226446c5e898be0be470088b2
Nikhil <nik.ranchi7@gmail.com>
2021-12-19 23:11:04 +0530

    First Commit

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('I am Stanish!')
+    print_hi('I am Stanish!.')
+    #The guy is very cool.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
